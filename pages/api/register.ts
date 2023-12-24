d7f915a34df35612527d3f62179c6da7e37cc8be
tapiwa madzanise <tmadz8779@gmail.com>
2023-12-24 19:46:44 +0200

    fixed something else

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -6,10 +6,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "POST") {
-    return res.status(405).end();
-  }
-
   try {
     const { email, name, password } = req.body;
 
